94b8d26b891334e35d059a683ce7549d6866ced9
stavros-kladis <svkladis@gmail.com>
2026-06-23 13:55:22 +0300

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,14 @@
+msg1, msg2 = "World", "Hello"
+msg3, msg4, msg5 = "Name", "Is", "My"
+# Don't change the code above this line
+msg1, msg2 = "Hello", "World"
+msg3, msg4, msg5 = "My", "Name", "Is" 
+
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
